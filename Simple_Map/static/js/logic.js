9623eff94aea4d64e916140a0953fc7116076e07
Zhengyu0913 <fanzhengyu1995@outlook.com>
2022-03-20 13:43:27 -0500

    update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,13 +3,13 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
-
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
   // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -20,3 +20,25 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+// L.polyline(line, {
+//   color: "red"
+// }).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
